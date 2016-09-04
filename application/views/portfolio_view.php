00d6f95463924e70343df0a2c99e239af9cd7a80
Leo_Sky <vasya_90@list.ru>
2016-09-04 16:45:28 +1000

    hail Hydra

---
+++ application/views/portfolio_view.php
@@ -0,0 +1,18 @@
+<h1>Список членов организации</h1>
+<p>
+<table>
+<tr><td>Имя</td><td>Возраст</td><td>Сообщение</td></tr>
+<?php
+
+//print_r($data);
+//	foreach($data as $row)
+//	{
+//		echo '<tr><td>'.$row['Year'].'</td><td>'.$row['Site'].'</td><td>'.$row['Description'].'</td></tr>';
+//	}
+//
+
+
+
+?>
+</table>
+</p>
---
+++ application/views/services_view.php
@@ -0,0 +1,59 @@
+<?php
+require_once ('D:\openServer\OpenServer\domains\vitalyswipe\php\function.php');
+
+if (isset($_POST['add_user'])){
+    if(empty($_POST['user_name']) || empty($_POST['user_age']) || empty($_POST['user_message'])) exit ("Данные отсутствуют");
+    $link=connect();
+    addQuery($_POST["user_name"], $_POST["user_age"],$_POST["user_message"], $name_in_db, $link);
+    mysqli_close ($link);
+}
+?>
+
+<div class="col-md-12">
+  <div class="well" style="margin-top: 0%; background-color:lightgrey">
+        <h3 style="text-align: center;">Регистрационные данные</h3>
+        <form role="form" id="contactForm"  class="shake" action="services_view.php" method="POST" enctype="multipart/form-data">
+            <!-- Имя -->
+            <div class="row">
+                <div class="form-group col-sm-12">
+                    <label for="name" class="h4">Имя</label>
+                    <input name="user_name" type="text" class="form-control" id="name" placeholder="Введите свое имя" data-error="Забыли как вас зовут?" required pattern="^[А-Яа-яЁё\s]+$">
+                    <div class="help-block with-errors"></div>
+                </div>
+            </div>
+
+            <!--Возраст-->
+            <div class="row">
+                <div class="form-group col-sm-12">
+                    <label for="age" class="h4">Возраст</label>
+                    <input name="user_age" type="text" class="form-control" id="age" placeholder="Введите ваш возраст" data-error="Забыли сколько вам лет?" required pattern="^[ 0-9]+$" >
+                    <div class="help-block with-errors"></div>
+                </div>
+            </div>
+
+            <!--Картинка-->
+            <div class="row">
+                <div class="form-group col-sm-12">
+                    <label for="fileToUpload" class="h4">Загрузить картинку</label>
+            <input type="file" class="file" name="fileToUpload" id="fileToUpload">
+                </div>
+            </div>
+
+            <!-- О себе -->
+            <div class="row">
+            <div class="form-group col-sm-12">
+                <label for="message" class="h4">О себе</label>
+                <textarea name="user_message" id="message" class="form-control" rows="5" placeholder="Напишите немного о себе"  data-error="Необходимо написть хоть что-нибудь" required></textarea>
+            </div>
+            </div>
+
+            <!--Кнопка-->
+            <div class="row">
+                <div class="form-group col-sm-12 col-sm-offset-0">
+                    <input type="submit" class="col-xs-12 btn btn-md btn-success" value="Зарегистрироваться" name="add_user">
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </form>
+    </div>
+</div>
