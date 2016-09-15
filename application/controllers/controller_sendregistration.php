371b2057ddc7263c188aaeb96a112269861e739c
Leopold Skyer <ChernovIU@gmail.ru>
2016-09-16 09:36:42 +1000

    upload image SUCCESS

--- application/controllers/controller_sendregistration.php
+++ application/controllers/controller_sendregistration.php
@@ -22,19 +22,35 @@ Class controller_sendregistration extends Controller{
     {
         if (isset($_POST['user_name']) && ($_POST['user_age']) && ($_POST['user_message'])) {
             $stmt = $this->pdo->prepare("INSERT INTO Registration_Data (name, age, text) VALUES (:name, :age, :message)");
-
             $stmt->execute(array(
                 "name" => $_POST['user_name'],
                 "age" => $_POST['user_age'],
                 "message" => $_POST['user_message']
             ));
 
-            $stmt2 = $this->pdo->prepare("INSERT INTO Image_Data (user_id) SELECT id FROM Registration_Data WHERE NAME = :user_name");
-            $stmt2->bindParam(':user_name', $_POST['user_name']);
+            if (isset($_FILES['fileToUpload'])){
+                echo "файл не был загружен";
+            }else{
+                $path = 'photos/';
+                $ext = array_pop(explode('.', $_FILES['fileToUpload']['image'])); // расширение
+                $new_name = time() . '.' . $ext;
+                $full_path = $path . $new_name;
+                $name_in_db = substr($new_name, 0, -1);
 
+                if ($_FILES['fileToUpload']['error'] == 0) {
+                    if (substr($_FILES["fileToUpload"]["name"], -3) == "jpg" || substr($_FILES["file"]["name"], -3) == "png") {
+                        if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $full_path)) {
+                            echo "Регистрация прошла успешно";
+                        }
+                    }
+                }
+            }
+
+            $stmt2 = $this->pdo->prepare("INSERT INTO Image_Data (user_id, image) VALUES ((SELECT id FROM Registration_Data WHERE NAME = :user_name), '".$name_in_db."')");
+            $stmt2->bindParam(':user_name', $_POST['user_name']);
             $stmt2->execute();
 
-
+            
             $controller_name = 'application/controllers/controller_sendregistration.php';
             $str = strtolower($controller_name);
             new Controller_sendregistration($this->action_index());
